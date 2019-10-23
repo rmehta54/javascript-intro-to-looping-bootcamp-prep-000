30e19d5dfaaaf70ec03808263c8c15029bcffc59
rajmehta54 <rajmehta54@gmail.com>
2019-10-23 23:31:22 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array1) 
+{
+for (let i = 0; i < 25; i++)
+  {
+  if (i = 1) {
+    array1.push(`I am ${i} strange loop.`)
+  }
+  else {
+    array1.push(`I am ${i} strange loops.`)
+  }
+}
+
+}
+
