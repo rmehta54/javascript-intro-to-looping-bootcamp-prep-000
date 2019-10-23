6892e55a5d410eeb0bd649d50d6abbf99740c385
rajmehta54 <rajmehta54@gmail.com>
2019-10-23 23:33:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,14 +1,11 @@
-function forLoop(array1) 
+function forLoop(array1)
 {
-for (let i = 0; i < 25; i++)
+  for (let i = 0; i < 25; i++)
   {
-  if (i = 1) {
-    array1.push(`I am ${i} strange loop.`)
-  }
-  else {
-    array1.push(`I am ${i} strange loops.`)
+    if (i = 1)
+    {
+      array1.push(`I am ${i} strange loop.`)
+    }
+    else
   }
 }
-
-}
-
